195d8b43da3b483ec5e54dd474cbfe74778fbfa8
SebastianDrozd <37122892+SebastianDrozd@users.noreply.github.com>
2021-06-15 16:49:52 -0500

    Setup Command Repo and command model

---
+++ Data/ICommanderRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LesJacksonApiTutorial.Models;
+
+namespace LesJacksonApiTutorial.Data
+{
+    interface ICommanderRepo
+    {
+        public Command getCommand(int id);
+
+        public List<Command> getCommands();
+    }
+}
---
+++ Models/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LesJacksonApiTutorial.Models
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        public string HowTO { get; set; }
+        public string Line { get; set; }
+        public string Platform { get; set; }
+    }
+}
